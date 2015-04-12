96da0595ea99a8ca0fa00e20eb02a8312bfce4db
gastonrqn <gaston.rqn@gmail.com>
2015-04-12 15:28:42 -0300

    Ejercicios 1,5,7

--- TP1/src/Grafo.hs
+++ TP1/src/Grafo.hs
@@ -9,8 +9,11 @@ instance (Show a) => Show (Grafo a) where
 -- ---------------------------------Sección 3--------- Grafos ---------------------------
 
 -- Ejercicio 1
+-- Crea un nuevo grafo con una lista vacía de nodos y una función que
+-- devuelve siempre "undefined" (porque no hay nodos, y el dominio de
+-- la función de ejes es el conjunto de nodos).
 vacio :: Grafo a
-vacio = undefined
+vacio = G [] (const undefined)
 
 -- Ejercicio 2
 nodos :: Grafo a -> [a]
@@ -25,16 +28,30 @@ agNodo :: a -> Grafo a -> Grafo a
 agNodo = undefined
 
 -- Ejercicio 5
+-- Construye un nuevo grafo:
+--    * Filtra la lista de nodos para sacar el nodo.
+--    * Crea una nueva función que se indefine para el nodo que acabamos
+--      de sacar y devuelve lo mismo que antes para los demás nodos.
 sacarNodo :: a -> Grafo a -> Grafo a
-sacarNodo = undefined
+sacarNodo n (G nodos ejes) = G (filter (/=n) nodos) (\x -> if (x==n) then undefined else (ejes x))
 
 -- Ejercicio 6
 agEje :: (a,a) -> Grafo a -> Grafo a
 agEje = undefined
 
 -- Ejercicio 7
+-- Si la lista es vacía, crea un grafo vacío.
+-- Si la lista no es vacía, recursivamente va agregando nodos al grafo,
+-- y a partir del segundo nodo que agrega, también va agregando ejes.
+-- Es por eso que en el paso inductivo primero pregunta si el grafo
+-- "rec" ya tiene algún nodo y si es así agrega un eje entre el último
+-- nodo agregado y el que se está agregando en este paso.
 lineal :: [a] -> Grafo a
-lineal = undefined
+lineal = foldr (\n rec ->  if null (nodos rec)
+						   then agNodo n rec
+						   else agEje (n,head (nodos rec)) (agNodo n rec)
+			   )
+			   vacio
 
 -- Ejercicio 8
 union :: Grafo a -> Grafo a -> Grafo a
