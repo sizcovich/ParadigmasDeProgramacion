29c08214d4b0013aa27608f06e1a5cf4032d9119
Sabrina <sizcovich@gmail.com>
2015-04-12 21:10:55 -0300

    ups

--- TP1/src/Grafo.hs
+++ TP1/src/Grafo.hs
@@ -51,9 +51,9 @@ sacarNodo n (G nodos ejes) = G (filter (/=n) nodos) (\x -> if (x==n) then undefi
 -- Se considera que el primer nodo pertenece al grafo.
 -- En primer lugar, se verifica si el segundo nodo está en el grafo. Si esto no ocurre se devuelve la función original, 
 -- sino se lo agrega.
-agEje :: (a,a) -> Grafo a -> Grafo a
+agEje :: Eq a => (a,a) -> Grafo a -> Grafo a
 agEje (x, y) (G ns t) = if y `elem` (t x) then (G ns t) else 
-									(G ns (\n -> if n == x then y:(t x) else (t n))
+									(G ns (\n -> if n == x then y:(t x) else (t n)))
 
 -- Ejercicio 7
 -- Si la lista es vacía, crea un grafo vacío.
